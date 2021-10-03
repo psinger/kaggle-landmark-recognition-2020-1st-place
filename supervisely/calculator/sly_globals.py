3759ed998a331b8b51b3df74aead7f97066c4516
kashkarov <kashkarov@dedicorp.ru>
2021-10-03 18:40:03 +0300

    calculator save embeddings

--- supervisely/calculator/sly_globals.py
+++ supervisely/calculator/sly_globals.py
@@ -9,8 +9,12 @@ team_id = os.environ["context.teamId"]
 workspace_id = os.environ["context.workspaceId"]
 device = os.environ['context.deviceId']
 
+local_dataset_path = os.path.join(my_app.data_dir, 'sly_dataset')
 remote_weights_path = os.environ['modal.state.slyFile']
 remote_embeddings_dir = os.environ['modal.state.slyEmbeddingsDir']
+download_batch_size = os.environ['modal.state.downloadBatchSize']
+calc_batch_size = os.environ['modal.state.batchSize']
 only_current_workspace = int(os.environ['modal.state.OnlyCurrentWorkspace'])  # 0 or 1
+
 # DEBUG
 sly.fs.clean_dir(my_app.data_dir, ignore_errors=True)
