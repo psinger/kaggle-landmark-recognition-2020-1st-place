3063234af0f5e79e4c53ba3f3bc9d5c3fe73f1ec
Daniel Yarockii <33942379+Lascarfo@users.noreply.github.com>
2021-10-27 12:15:08 +0300

    Update sly_globals.py

--- supervisely/serve/src/sly_globals.py
+++ supervisely/serve/src/sly_globals.py
@@ -30,7 +30,6 @@ pretrained_models_table = ast.literal_eval(os.environ['modal.state.models'])
 
 logger.info(pretrained_models_table)
 logger.info(type(pretrained_models_table))
-logger.info(json.loads(pretrained_models_table))
 
 # pretrained_models_table = list(json.loads(str(os.environ['modal.state.models'])))  # debug
 
