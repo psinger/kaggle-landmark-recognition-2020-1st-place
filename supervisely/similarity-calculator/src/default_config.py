44223f3d2b03840b32c202c3a5bbf587159ee388
Daniil Yarockii <yaroc.d@gmail.com>
2021-10-29 15:40:44 +0300

    ready to use

---
+++ supervisely/similarity-calculator/src/default_config.py
@@ -0,0 +1,89 @@
+import os
+import albumentations as A
+
+abs_path = os.path.dirname(__file__)
+
+args = {
+    'model_path': '/root/gld_pd/models/',
+    'data_path': '/root/pictures_data/inference/',
+    'data_path_2019': '/root/pictures_data/inference/',
+    'valid_csv_fn': 'test_filtered.csv',
+    'train_csv_fn': 'train_filtered.csv',
+
+    'gpus': '0',
+    'filter_warnings': True,
+    'logger': 'tensorboard',
+    'num_sanity_val_steps': 0,
+
+    'distributed_backend': 'ddp',
+    'channels_last': False,
+
+    'gradient_accumulation_steps': 2,
+    'precision': 16,
+    'sync_batchnorm': False,
+
+    'seed': 1138,
+    'num_workers': 0,
+    'save_weights_only': True,
+
+    'p_trainable': True,
+
+    'resume_from_checkpoint': None,
+    'pretrained_weights': None,
+
+    'normalization': 'imagenet',
+    'crop_size': 448,
+
+    'backbone': 'gluon_seresnext101_32x4d',
+    'embedding_size': 512,
+    'pool': 'gem',
+    'arcface_s': 45,
+    'arcface_m': 0.4,
+
+    'neck': 'option-D',
+    'head': 'arc_margin',
+
+    'crit': "bce",
+    'loss': 'arcface',
+    # 'focal_loss_gamma': 2,
+    'class_weights': "log",
+    'class_weights_norm': 'batch',
+
+    'optimizer': "sgd",
+    'weight_decay': 1e-4,
+    'lr': 0.01,
+    'batch_size': 32,
+    'test_batch_size': 40,
+
+    'max_epochs': 6,
+    'scheduler': {"method": "cosine", "warmup_epochs": 1},
+
+    'n_classes': 83,
+    'data_frac': 1.,
+
+    'neptune_project': 'xx/kaggle-landmark',
+}
+
+args['tr_aug'] = A.Compose([
+    # A.Resize(height=448, width=448),
+    A.SmallestMaxSize(512),
+    A.ColorJitter(brightness=(0., 0.5), contrast=(0., 0.5), saturation=(0., 0.5), hue=0.2, always_apply=False, p=0.5),
+    A.Rotate((-10, 10)),
+    A.RandomCrop(height=args['crop_size'], width=args['crop_size'], p=1.),
+    A.RandomScale(scale_limit=(0.5, 1.5), interpolation=1, always_apply=False, p=1.),
+    A.Perspective(scale=(0.05, 0.1), keep_size=False, p=0.7),
+    A.Resize(height=448, width=448),
+    A.HorizontalFlip(p=0.5),
+])
+
+args['val_aug'] = A.Compose([
+    # A.Resize(height=448, width=448),
+    A.SmallestMaxSize(512),
+    A.CenterCrop(height=args['crop_size'], width=args['crop_size'], p=1.)
+])
+
+args['test_aug'] = A.Compose([
+    A.Resize(height=448, width=448),
+    # A.SmallestMaxSize(512),
+    # A.CenterCrop(height=args['crop_size'], width=args['crop_size'], p=1.)
+])
---
+++ supervisely/similarity-calculator/src/main.py
@@ -0,0 +1,128 @@
+import os
+
+import supervisely_lib as sly
+
+import json
+import numpy as np
+
+import sly_globals as g
+import sly_functions as f
+
+
+@g.my_app.callback("load_embeddings_to_memory")
+@sly.timeit
+@g.my_app.ignore_errors_and_show_dialog_window()
+def load_embeddings_to_memory(api: sly.Api, task_id, context, state, app_logger):
+    try:
+        if state['selectAllEmbeddings']:
+            embeddings_info = g.api.app.get_field(g.task_id, 'data.embeddingsInfo')
+            embeddings_paths = np.hstack(list(embeddings_info.values()))
+        else:
+            embeddings_info = state['selectedEmbeddings']
+            embeddings_paths = np.hstack(list(embeddings_info))
+    except Exception as ex:
+        fields = [
+            {"field": f"state.loadingEmbeddings", "payload": False}
+        ]
+        g.api.task.set_fields(g.task_id, fields)
+        raise ValueError('No embedding files found. Please reselect embeddings.')
+
+    local_pickles_paths = f.download_embeddings(embeddings_paths)
+    g.embeddings_in_memory = f.load_embeddings_to_memory(local_pickles_paths)
+
+    embeddings_stats = {
+        'Embeddings Count': len(g.embeddings_in_memory['embedding']),
+        'Labels Num': len(set(g.embeddings_in_memory['label']))
+    }
+
+    fields = [
+        {"field": f"state.embeddingsLoaded", "payload": True},
+        {"field": f"state.loadingEmbeddings", "payload": False},
+        {"field": f"data.embeddingsStats", "payload": embeddings_stats}
+    ]
+    g.api.task.set_fields(g.task_id, fields)
+
+
+@g.my_app.callback("clear_fields")
+def clear_fields(api: sly.Api, task_id, context, state, app_logger):
+    fields = [
+        {"field": f"state.selectAllEmbeddings", "payload": True},
+        {"field": f"state.selectedEmbeddings", "payload": []},
+        {"field": f"state.embeddingsLoaded", "payload": False},
+    ]
+    g.api.task.set_fields(g.task_id, fields)
+
+
+@g.my_app.callback("select_checkpoint")
+@sly.timeit
+def select_checkpoint(api: sly.Api, task_id, context, state, app_logger):
+    selected_weights_type = state['modelWeightsOptions']
+    if selected_weights_type == 'pretrained':
+        listed_path = os.path.join(g.remote_embeddings_dir, state['selectedModel'])
+        projects_list_for_checkpoint = g.api.file.list(g.team_id, listed_path)
+        files_paths = [current_checkpoint['path'] for current_checkpoint in projects_list_for_checkpoint]
+
+        filtered_paths = f.filter_paths_by_workspace_id(files_paths)
+        pickles_files_paths = [current_path for current_path in filtered_paths if current_path.endswith('.pkl')]
+        paths_by_projects = f.group_paths_by_project_ids(pickles_files_paths)
+
+    else:
+        paths_by_projects = None
+        pass
+
+    fields = [
+        {"field": f"state.checkpointSelected", "payload": True},
+        {"field": f"state.loadingEmbeddingsList", "payload": False},
+        {"field": f"data.embeddingsInfo", "payload": paths_by_projects},
+    ]
+
+    g.api.task.set_fields(g.task_id, fields)
+
+
+@g.my_app.callback("calculate_similarity")
+@sly.timeit
+def calculate_similarity(api: sly.Api, task_id, context, state, app_logger):
+    g.logger.info(f'calculating similarity for batch; {context["request_id"]}')
+
+    data_to_process = dict(state['input_data'])
+
+    top_k = data_to_process.get('top_k', 5)
+    input_embeddings = data_to_process['embeddings']
+
+    input_embeddings = np.asarray(input_embeddings).astype(np.float32)
+    embeddings_in_memory = np.asarray(g.embeddings_in_memory["embedding"]).astype(np.float32)
+
+    pred_dist, pred_index_of_labels = f.get_topk_cossim(input_embeddings,
+                                                        embeddings_in_memory,
+                                                        k=top_k,
+                                                        device='cpu')
+
+    indexes_to_labels = np.asarray(g.embeddings_in_memory['label'])
+
+    pred_dist = [list(curr_row) for curr_row in list(pred_dist.data.cpu().numpy())]
+    pred_labels = [list(indexes_to_labels[list(curr_row)]) for curr_row in
+                   list(pred_index_of_labels.data.cpu().numpy())]
+
+    output_data = json.dumps(str({'pred_dist': pred_dist,
+                                  'pred_labels': pred_labels}))
+
+    request_id = context["request_id"]
+    g.my_app.send_response(request_id, data=output_data)
+
+    g.logger.info(f'successfully calculated! {context["request_id"]}')
+
+
+def main():
+    sly.logger.info("Similarity calculator started")
+    data = {}
+    state = {}
+    data["taskId"] = g.task_id
+
+    f.init_fields(state=state, data=data)
+    f.init_progress_bars(state=state, data=data)
+
+    g.my_app.run(data=data, state=state)
+
+
+if __name__ == "__main__":
+    sly.main_wrapper("main", main)
